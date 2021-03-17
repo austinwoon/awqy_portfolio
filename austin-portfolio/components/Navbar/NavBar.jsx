5b889052f309a7e0547cb1fe2308e95bf4f861e2
austinwoon <austinwoon.2017@smu.edu.sg>
2021-03-17 16:05:41 +0800

    Initial Commit

---
+++ austin-portfolio/components/Navbar/NavBar.jsx
@@ -0,0 +1,67 @@
+import { Link, Flex, Heading, Box, Spacer } from '@chakra-ui/layout';
+import React from 'react';
+
+const NavBar = () => {
+    return (
+      <Flex
+        width="100vw"
+        justify={"space-around"}
+        padding={"24px 0px 24px 0px"}
+        maxWidth={"50vw"}
+        >
+        <Box>
+          <Link
+            fontSize={"24"}
+            fontWeight={"bold"}
+            padding={"8px"}
+            borderRadius="8px"
+            _hover={{
+              backgroundColor: "blue.900",
+              color: "white",
+              transition: "0.35s ease"
+            }}
+          >
+            About
+          </Link>
+        </Box>
+
+        {/*<Spacer />*/}
+
+        <Box>
+          <Link
+            fontSize={"24"}
+            fontWeight={"bold"}
+            padding={"8px"}
+            borderRadius="8px"
+            _hover={{
+              backgroundColor: "blue.900",
+              color: "white",
+              transition: "0.35s ease"
+            }}
+          >
+            Works
+          </Link>
+        </Box>
+
+        {/*<Spacer />*/}
+
+        <Box>
+          <Link
+            fontSize={"24"}
+            fontWeight={"bold"}
+            padding={"8px"}
+            borderRadius="8px"
+            _hover={{
+              backgroundColor: "blue.900",
+              color: "white",
+              transition: "0.35s ease"
+            }}
+          >
+            Contact
+          </Link>
+        </Box>
+      </Flex>
+    )
+}
+
+export default NavBar;
---
+++ austin-portfolio/constants/introduction.js
@@ -0,0 +1,2 @@
+export const introduction = `Hello! I’m Austin, a Software Engineer in Sunny Singapore. 
+My main focus of expertise is Full-stack Web Development and most of my projects involve the ERN stack (Express, React, Node.JS).`
