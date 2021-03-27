7783d0820b17b3f2262b6a21baf427d6084f3a6e
austinwoon <austinwoon.2017@smu.edu.sg>
2021-03-27 21:51:05 +0800

    Updated navbar css & shifted scroll state to navbar

--- austin-portfolio/pages/index.js
+++ austin-portfolio/pages/index.js
@@ -14,22 +14,9 @@ const DynamicPortfolioSection = dynamic(() =>
 
 export default function Home() {
     const [isMobile] = useMediaQuery(['(max-width: 900px)']);
-    const [scrolled, setScrolled] = React.useState(false);
+
     const aboutRef = React.useRef();
     const portfolioRef = React.useRef();
-
-    const handleScroll = () => {
-        const offset = window.scrollY;
-        if (offset > 50) {
-            setScrolled(true);
-        } else {
-            setScrolled(false);
-        }
-    };
-
-    React.useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-    });
 
     return (
         <MediaQueryContext.Provider value={{ isMobile }}>
@@ -37,19 +24,7 @@ export default function Home() {
                 <title>Austin Portfolio</title>
             </Head>
             <Box bgGradient="linear-gradient(190deg, brand.bgWhite, hsla(0,0%,45%,0) 80%)">
-                <Box
-                    zIndex={5}
-                    className={scrolled && 'glassMorphism'}
-                    position={'sticky'}
-                    top="0"
-                    borderBottomLeftRadius={['8px', '8px', '10px', '10px']}
-                    borderBottomRightRadius={['8px', '8px', '10px', '10px']}
-                    pt={['0.25vh', '0.5vh', '1vh', '1vh']}
-                    pb={['0.25vh', '0.5vh', '1vh', '1vh']}
-                    w={'100%'}
-                >
-                    <NavBar refs={{ aboutRef, portfolioRef }} />
-                </Box>
+                <NavBar refs={{ aboutRef, portfolioRef }} />
 
                 <Element id={'about'} ref={aboutRef}>
                     <DynamicIntro />
